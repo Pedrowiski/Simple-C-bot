65dd10a7e3959773c6ed81d1a60867b3ef48ce7a
Pedrowiski <73365247+Pedrowiski@users.noreply.github.com>
2021-11-28 14:23:40 -0300

    A lot of changes and improvements.

--- bot.c
+++ bot.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include "nxjson.h" //https://github.com/thestr4ng3r/nxjson
+#include <nxjson.h> //https://github.com/thestr4ng3r/nxjson
 #include <curl/curl.h>
 
 //=================================================================//
@@ -28,6 +28,9 @@
 #define LOG_ERR(string) \
     log_error(string);  \
     return
+
+#define EXEC_COMMAND(command) \
+    command(client, bot, message)
 
 //=================================================================//
 // Auxiliar functions
@@ -75,8 +78,8 @@ char *split_string(char *string, char *delimiter, int position)
 }
 
 /*
-* https://stackoverflow.com/questions/24442459/returning-formatted-string-from-
-* c-function
+* https://stackoverflow.com/questions/24442459/returning-formatted-string-
+* from-c-function
 */
 
 char *buffer_vsnprintf(char *format, ...)
@@ -101,6 +104,23 @@ char *buffer_vsnprintf(char *format, ...)
     return buffer;
 }
 
+char *json_property_exists(struct nx_json *json,
+    char *property,
+    char *string)
+{
+    const struct nx_json *json_struct = nx_json_get(json, property);
+
+    if (json_struct == NX_JSON_NULL) {
+        nx_json_free(json_struct);
+        return string;
+    } else {
+        char *new_buffer = buffer_vsnprintf((char *) json_struct->text_value);
+        nx_json_free(json_struct);
+
+        return new_buffer;
+    }
+}
+
 //=================================================================//
 // CURL
 //=================================================================//
@@ -109,12 +129,6 @@ struct data_struct {
     char *response;
     int  size;
 };
-
-void init_data_struct(struct data_struct *self)
-{
-    self->size     = 0;
-    self->response = calloc(sizeof(char), self->size);
-}
 
 size_t curl_write_handler(void *buffer,
     size_t size,
@@ -129,6 +143,99 @@ size_t curl_write_handler(void *buffer,
     user_data->response[real_size] = '\0';
 
     return real_size;
+}
+//=================================================================//
+// Command functions
+//=================================================================//
+
+void ping(struct discord *client,
+    const struct discord_user *bot,
+    const struct discord_message *message)
+{
+    SEND_TEXT_MESSAGE(buffer_vsnprintf("_Pong_, %s! 🏓",
+    message->author->username));
+    free(parameters.content);
+}
+
+void ask(struct discord *client,
+    const struct discord_user *bot,
+    const struct discord_message *message)
+{
+    char *answers[] = {
+        "Sim",
+        "Não",
+        "Talvez",
+        "Claro",
+        "Não, com certeza.",
+        "Não sei"
+    };
+
+    int random_number = generate_random_number(ARRAY_SIZE(answers));
+    SEND_TEXT_MESSAGE(answers[random_number]);
+}
+
+void apod(struct discord *client,
+    const struct discord_user *bot,
+    const struct discord_message *message)
+{
+    const char *brute_url = "https://api.nasa.gov/planetary/apod?api_key=";
+    const char *api_key = "";
+    char *url = buffer_vsnprintf("%s%s", brute_url, api_key);
+
+    CURL *curl = curl_easy_init();
+
+    if (curl == NULL) {
+        LOG_ERR("Ocorreu um erro ao tentar iniciar o CURL!");
+    }
+
+    struct data_struct json_response;
+    memset(&json_response, 0, sizeof(json_response));
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_handler);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &json_response);
+    CURLcode response = curl_easy_perform(curl);
+
+    if (response != CURLE_OK) {
+        LOG_ERR("Falha ao requisitar o JSON!");
+    }
+
+    const nx_json *json = nx_json_parse(json_response.response, 0);
+
+    if (json == NULL) {
+        LOG_ERR("Falha em parsear o JSON!");
+    }
+
+    struct discord_embed embed = {
+        .color = 0x2a9df4,
+        .url = (char *) nx_json_get(json, "hdurl")->text_value,
+
+        .image = &(struct discord_embed_image) {
+            .url = (char *) nx_json_get(json, "url")->text_value
+        },
+
+        .title = (char *) nx_json_get(json, "title")->text_value,
+
+        .description = (char *) nx_json_get(json,
+            "explanation")->text_value,
+
+        .footer = &(struct discord_embed_footer) {
+            .text =  buffer_vsnprintf("Image author: %s",
+                json_property_exists((struct nx_json *) json,
+                    "copyright",
+                    "Unknown author"))
+         }
+     };
+
+     struct discord_create_message_params params = {
+         .embed = &embed
+     };
+
+     discord_create_message(client, message->channel_id, &params, NULL);
+     nx_json_free(json);
+
+     free(embed.footer->text);
+     free(url);
 }
 
 //=================================================================//
@@ -157,72 +264,11 @@ void on_message(struct discord *client,
         return;
 
     if (starts_with("&ping", message->content) == 0) {
-        SEND_TEXT_MESSAGE(buffer_vsnprintf("_Pong_, %s! 🏓",
-            message->author->username));
-        free(parameters.content);
+        EXEC_COMMAND(ping);
     } else if (starts_with("&ask", message->content) == 0) {
-        char *answers[] = {
-            "Sim",
-            "Não",
-            "Talvez",
-            "Claro",
-            "Não sei"
-        };
-
-        int random_number = generate_random_number(ARRAY_SIZE(answers));
-        SEND_TEXT_MESSAGE(answers[random_number]);
+        EXEC_COMMAND(ask);
     } else if (starts_with("&apod", message->content) == 0) {
-        const char *brute_url = "https://api.nasa.gov/planetary/apod?api_key=";
-        const char *api_key = "";
-        const char *url = buffer_vsnprintf("%s%s", brute_url, api_key);
-
-        CURL *curl = curl_easy_init();
-
-        if (curl == NULL) {
-            LOG_ERR("Ocorreu um erro ao tentar iniciar o curl!");
-        }
-
-        struct data_struct json_response;
-        memset(&json_response, 0, sizeof(json_response));
-
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_handler);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &json_response);
-        CURLcode response = curl_easy_perform(curl);
-
-        if (response != CURLE_OK) {
-            LOG_ERR("Falha ao requisitar o JSON");
-        }
-
-        const nx_json *json = nx_json_parse(json_response.response, 0);
-
-        struct discord_embed embed = {
-            .color = 0x2a9df4,
-            .url = (char *) url,
-
-            .image = &(struct discord_embed_image) {
-                .url = (char *) nx_json_get(json, "url")->text_value
-            },
-
-            .title = (char *) nx_json_get(json, "title")->text_value,
-
-            .description = (char *) nx_json_get(json,
-                "explanation")->text_value,
-
-            .footer = &(struct discord_embed_footer) {
-                .text =  buffer_vsnprintf("Image author: %s",
-                    nx_json_get(json, "copyright")->text_value)
-            }
-
-        };
-
-        struct discord_create_message_params params = {
-            .embed = &embed
-        };
-
-        discord_create_message(client, message->channel_id, &params, NULL);
-        nx_json_free(json);
-        free(embed.footer->text);
+        EXEC_COMMAND(apod);
     }
 }
 
@@ -233,6 +279,9 @@ int main(void)
     discord_set_on_ready(client, on_ready);
     discord_set_on_message_create(client, on_message);
     discord_run(client);
+
+    discord_cleanup(client);
+    discord_global_cleanup();
 
     return 0;
 }
